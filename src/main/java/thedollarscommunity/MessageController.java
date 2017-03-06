f083e0d4ab9e47a334f0d18e0cb00870cdc1f4b4
Emmanuel <emmanuelbendavid@gmail.com>
2017-03-06 17:25:55 -0300

    Still trying

--- src/main/java/thedollarscommunity/MessageController.java
+++ src/main/java/thedollarscommunity/MessageController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/globalchat")
+@RequestMapping("/message")
 public class MessageController {
 
 	private MessageRepository repository;
