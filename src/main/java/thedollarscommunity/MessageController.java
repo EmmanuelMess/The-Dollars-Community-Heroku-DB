8e94ef20f8893b2c526f8fcae4f65d7ffae835d9
Emmanuel <emmanuelbendavid@gmail.com>
2017-03-06 18:37:06 -0300

    Sever writes time now

--- src/main/java/thedollarscommunity/MessageController.java
+++ src/main/java/thedollarscommunity/MessageController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 @RestController
@@ -29,6 +31,8 @@ public class MessageController {
 
 	@RequestMapping(value = "/new", method = RequestMethod.POST)
 	public ResponseEntity<Message> update(@RequestBody Message message) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy:MM:dd'T'HH:mm:ss'.0+00:00'");
+		message.setTime(sdf.format(new Timestamp(System.currentTimeMillis())));
 		repository.save(message);
 		return get(message.getId());
 	}
